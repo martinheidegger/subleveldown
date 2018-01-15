387f8882d472d2996a9389ea6729d4cba0760ae0
Martin Heidegger <martin.heidegger@gmail.com>
2018-01-15 16:43:31 +0900

    Updated dependencies & applied necessary code updates

--- leveldown.js
+++ leveldown.js
@@ -1,12 +1,12 @@
 var util = require('util')
-var abstract = require('abstract-leveldown')
+var encoding = require('encoding-down')
 var wrap = require('level-option-wrap')
 
-var END = new Buffer([0xff])
+var END = Buffer.from([0xff])
 
 var concat = function (prefix, key, force) {
   if (typeof key === 'string' && (force || key.length)) return prefix + key
-  if (Buffer.isBuffer(key) && (force || key.length)) return Buffer.concat([new Buffer(prefix), key])
+  if (Buffer.isBuffer(key) && (force || key.length)) return Buffer.concat([Buffer.from(prefix), key])
   return key
 }
 
@@ -30,8 +30,9 @@ SubIterator.prototype.end = function (cb) {
 
 var SubDown = function (db, prefix, opts) {
   if (!(this instanceof SubDown)) return new SubDown(db, prefix, opts)
+  opts = opts || {}
   if (typeof opts === 'string') opts = {separator: opts}
-  if (!opts) opts = {}
+  encoding.EncodingDOWN.call(this, [db, opts])
 
   var separator = opts.separator
 
@@ -41,8 +42,7 @@ var SubDown = function (db, prefix, opts) {
   if (prefix[prefix.length - 1] === separator) prefix = prefix.slice(0, -1)
 
   this.db = db
-  this.leveldown = null
-  this.prefix = separator + prefix + separator
+  var rawPrefix = separator + prefix + separator
   this._beforeOpen = opts.open
 
   var self = this
@@ -57,57 +57,66 @@ var SubDown = function (db, prefix, opts) {
     }
   }
 
-  abstract.AbstractLevelDOWN.call(this, 'no-location')
+  var onOpen = function () {
+    if (self.db.db.type === 'subdown' && self.db.db.prefix) {
+      self.prefix = self.db.db.prefix + rawPrefix
+      self.leveldown = self.db.db.leveldown
+    } else {
+      onClose()
+    }
+  }
+  var onClose = function () {
+    self.prefix = rawPrefix
+    self.leveldown = self.db.db
+  }
+
+  this.db.on('open', onOpen)
+  this.db.on('close', onClose)
+
+  if (this.db.isOpen()) {
+    onOpen()
+  } else {
+    onClose()
+  }
+
+  this.destroy = function () {
+    self.db.off('open', onOpen)
+    self.db.off('close', onClose)
+    self.db.destroy()
+  }
 }
 
-util.inherits(SubDown, abstract.AbstractLevelDOWN)
+util.inherits(SubDown, encoding.EncodingDOWN)
 
 SubDown.prototype.type = 'subdown'
 
+SubDown.prototype.isOpen = function () {
+  return this.db.isOpen()
+}
+
 SubDown.prototype._open = function (opts, cb) {
-  var self = this
-
-  if (this.db.isOpen()) {
-    if (this.db.db.type === 'subdown' && this.db.db.prefix) {
-      this.prefix = this.db.db.prefix + this.prefix
-      this.leveldown = this.db.db.leveldown
-    } else {
-      this.leveldown = this.db.db
-    }
-    return done()
-  }
-
-  this.db.on('open', this.open.bind(this, opts, done))
-
-  function done (err) {
-    if (err || !self._beforeOpen) return cb(err)
-    self._beforeOpen(cb)
-  }
+  this.db.open(cb)
 }
 
-SubDown.prototype.close = function () {
-  this.leveldown.close.apply(this.leveldown, arguments)
+SubDown.prototype._close = function () {
+  this.db.close.apply(this.db, arguments)
 }
 
-SubDown.prototype.setDb = function () {
-  this.leveldown.setDb.apply(this.leveldown, arguments)
-}
-
-SubDown.prototype.put = function (key, value, opts, cb) {
+SubDown.prototype._put = function (key, value, opts, cb) {
   this.leveldown.put(concat(this.prefix, key), value, opts, cb)
 }
 
-SubDown.prototype.get = function (key, opts, cb) {
+SubDown.prototype._get = function (key, opts, cb) {
   this.leveldown.get(concat(this.prefix, key), opts, cb)
 }
 
-SubDown.prototype.del = function (key, opts, cb) {
+SubDown.prototype._del = function (key, opts, cb) {
   this.leveldown.del(concat(this.prefix, key), opts, cb)
 }
 
 SubDown.prototype.batch =
 SubDown.prototype._batch = function (operations, opts, cb) {
-  if (arguments.length === 0) return new abstract.AbstractChainedBatch(this)
+  if (arguments.length === 0) return new encoding.EncodingBatch(this)
   if (!Array.isArray(operations)) return this.leveldown.batch.apply(null, arguments)
 
   var subops = new Array(operations.length)
@@ -116,19 +125,15 @@ SubDown.prototype._batch = function (operations, opts, cb) {
     subops[i] = {type: o.type, key: concat(this.prefix, o.key), value: o.value}
   }
 
-  this.leveldown.batch(subops, opts, cb)
+  return this.leveldown.batch(subops, opts, cb)
 }
 
-SubDown.prototype.approximateSize = function (start, end, cb) {
-  this.leveldown.approximateSize.apply(this.leveldown, arguments)
+SubDown.prototype._approximateSize = function (start, end, cb) {
+  return this.leveldown.approximateSize.apply(this.leveldown, arguments)
 }
 
 SubDown.prototype.getProperty = function () {
   return this.leveldown.getProperty.apply(this.leveldown, arguments)
-}
-
-SubDown.prototype.destroy = function () {
-  return this.leveldown.destroy.apply(this.leveldown, arguments)
 }
 
 SubDown.prototype.repair = function () {
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var memdown = require('memdown')
 var subdown = require('../leveldown')
 var levelup = require('levelup')
 var testCommon = require('./common')
-var testBuffer = new Buffer('this-is-test-data')
+var testBuffer = Buffer.from('this-is-test-data')
 
 require('abstract-leveldown/abstract/open-test').args(down, test, testCommon)
 require('abstract-leveldown/abstract/open-test').open(down, test, testCommon)
@@ -18,5 +18,5 @@ require('abstract-leveldown/abstract/iterator-test').all(down, test, testCommon)
 require('abstract-leveldown/abstract/ranges-test').all(down, test, testCommon)
 
 function down (loc) {
-  return subdown(levelup(loc, {db: memdown}), 'test')
+  return subdown(levelup(memdown()), 'test')
 }
